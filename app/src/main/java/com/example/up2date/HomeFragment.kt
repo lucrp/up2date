55d48940bc4032d3cfca4f4575572a1b268e211c
lucasrp <lcsrpaiva@gmail.com>
2021-05-07 13:30:43 +0200

    Add retrofit and moshi dependencies/Test RecyclerView

--- app/src/main/java/com/example/up2date/HomeFragment.kt
+++ app/src/main/java/com/example/up2date/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dfl.newsapi.NewsApiRepository
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -14,4 +15,5 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+    val newsApiRepository = NewsApiRepository("efd3cc55a5ea44de8b006d7848ef67b7")
 }
